f64f1ca45a9b9df0db7ec24f9b8b9f27a27062ee
abdusharip <abdusharipa9@gmail.com>
2025-02-05 02:26:34 +0500

    dinamik navigatsiya va logo hajmi kaydi

--- js/script.js
+++ js/script.js
@@ -58,3 +58,54 @@ window.addEventListener('DOMContentLoaded', () => {
 			})
 	})
 })
+
+// for dinamic nav
+window.addEventListener('DOMContentLoaded', () => {
+	const navLinks = document.querySelectorAll('#topNav .nav-link')
+	const sections = document.querySelectorAll('.section')
+
+	// Nav link bosilganda faollikni yangilash
+	navLinks.forEach(link => {
+		link.addEventListener('click', event => {
+			event.preventDefault() // Href linkga o'tmaslik uchun
+			const targetId = link.getAttribute('href').substring(1) // Id olish
+			const targetSection = document.getElementById(targetId)
+
+			// Sectionga scroll qilish
+			targetSection.scrollIntoView({ behavior: 'smooth' })
+
+			// Har bir linkni faollikdan chiqarish
+			navLinks.forEach(link => link.classList.remove('active'))
+
+			// Tanlangan linkni faollashtirish
+			link.classList.add('active')
+		})
+	})
+
+	// Scroll holatida faollikni yangilash
+	const changeActiveLink = () => {
+		let currentSection = null
+		sections.forEach(section => {
+			const rect = section.getBoundingClientRect()
+			if (
+				rect.top <= window.innerHeight / 2 &&
+				rect.bottom >= window.innerHeight / 2
+			) {
+				currentSection = section
+			}
+		})
+
+		if (currentSection) {
+			navLinks.forEach(link => {
+				if (link.getAttribute('href').substring(1) === currentSection.id) {
+					link.classList.add('active')
+				} else {
+					link.classList.remove('active')
+				}
+			})
+		}
+	}
+
+	window.addEventListener('scroll', changeActiveLink)
+	changeActiveLink() // Boshlang'ich holatni to'g'rilash
+})
